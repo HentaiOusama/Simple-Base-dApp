3c49c9cbf65f41d0ca0cc6c06fa8dbcda2291ea7
Hentai Ousama <1905612@kiit.ac.in>
2021-06-10 03:26:50 +0530

    Small UI Additions

--- app.js
+++ app.js
@@ -17,10 +17,18 @@ app.get('/socket.io.js', (req, res) => {
 
 app.use(express.static(__dirname + '/public'));
 
+let activeSocketConnections = 0;
+
 io.on('connection', (socket) => {
-    console.log('Socket connection made. Id : ', socket.id);
+    activeSocketConnections++;
+    console.log('Socket connection made. Id : ' + socket.id + ", Active Socket Connections : " + activeSocketConnections);
+
+    socket.on('disconnect', () => {
+        activeSocketConnections--;
+        console.log('Socket connection Closed. Active Socket Connections : ' + activeSocketConnections);
+    });
 });
 
-server.listen(4000, () => {
-    console.log('Listening to port 4000');
+server.listen(process.env.PORT, () => {
+    console.log('Listening to port ' + process.env.PORT);
 });
