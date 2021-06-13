8d4129999db4a57d137c275837f1a827420c0843
Hentai Ousama <1905612@kiit.ac.in>
2021-06-13 14:04:16 +0530

    Added working web3 framework.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import io from 'socket.io-client';
+import { Web3ComponentsService } from "./web3-components.service";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'blockchain-reader';
+  web: any
+  socket = io(window.location.origin);
+
+  constructor(_web3: Web3ComponentsService) {
+    this.web = _web3;
+  }
 }
---
+++ src/app/web3-components.service.ts
@@ -0,0 +1,20 @@
+import {Injectable} from '@angular/core';
+
+const Web3 = require('web3/dist/web3.min');
+
+declare let window: any;
+declare let require: any;
+
+@Injectable({
+    providedIn: 'root'
+})
+export class Web3ComponentsService {
+
+    constructor() {
+        if (typeof window.ethereum !== "undefined") {
+            window.web3 = new Web3(window.ethereum);
+        } else {
+            console.error('No Web3 Support Found. Please Consider Using MetaMask, etc');
+        }
+    }
+}
